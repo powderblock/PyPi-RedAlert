5104d6c3fa9397213ce84b15d4cc11b756cad8fb
Blade Nelson <powderblock@gmail.com>
2014-08-29 03:12:07 -0400

    Adds calls to feedparser. Adds updateRSS function

--- GPIO.py
+++ GPIO.py
@@ -1,19 +1,36 @@
 import RPi.GPIO as gpio
 import time
+import feedparser
 
 gpio.setmode(gpio.BCM)
 gpio.setup(14, gpio.OUT)
 
-on = False
+def updateRSS():
+    global redditRSS, date, title
+    redditRSS = feedparser.parse('http://www.reddit.com/r/pics/new/.rss')
+    title = redditRSS['entries'][0]['title']
+    date = redditRSS.entries[0].published
 
-def lightHandle():
-        if on is True:
+updateRSS()
+post = title+' Time and Date posted: '+date
+
+
+#Main loop.
+while(1):
+    updateRSS()
+
+    #Make a string out of some of the items in the RSS feed.
+    newPost = title+' Time and Date posted: '+date
+
+    #Check if the post has changed since last loop.
+    if post != newPost:
+        print("New post!\n")
+        print newPost
+        post = newPost
+        for i in range(1, 15):
+                gpio.output(14, gpio.LOW)
+                time.sleep(1)
                 gpio.output(14, gpio.HIGH)
 
-        if on is False:
-                gpio.output(14, gpio.LOW)
-
-while True:
-        math = raw_input()
-        math = math.lower()
-        if math == 'on':
+    else:
+        print("No new post.")
